b95418cd1ab45d69feec1b691f3e01e89a4d3900
kennyjhcheng <kennyjiahuacheng@gmail.com>
2021-02-06 16:38:23 -0800

    Added Settings

---
+++ WpfApp1/WpfApp1/Notification.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Notification.xaml
+    /// </summary>
+    public partial class Notification : Window
+    {
+        public Notification()
+        {
+            InitializeComponent();
+            Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() =>
+            {
+                var workingArea = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea;
+                var transform = PresentationSource.FromVisual(this).CompositionTarget.TransformFromDevice;
+                var corner = transform.Transform(new Point(workingArea.Right, workingArea.Bottom));
+
+                this.Left = corner.X - this.ActualWidth;
+                this.Top = corner.Y - this.ActualHeight;
+            }));
+        }
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void View_More_Click(object sender, RoutedEventArgs e)
+        {
+           JokesWindow jw = new JokesWindow();
+           jw.GoToPicsFromParent();
+           jw.Show();
+        }
+        private void Close_button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            this.Close();
+        }
+        public void UpdateImage(string url)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(url, UriKind.Absolute);
+            bitmap.EndInit();
+            Image.Source = bitmap;
+        }
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string path = "https://dog.ceo/api/breeds/image/random";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                dynamic picsResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(picsResponse);
+
+                Pic pic = JsonConvert.DeserializeObject<Pic>(picsResponse);
+                UpdateImage(pic.message);
+            }
+        }
+    }
+    
+    public class Pic
+    {
+        public string message { get; set; }
+        public string status { get; set; }
+    }
+
+}
---
+++ WpfApp1/WpfApp1/SettingWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Forms = System.Windows.Forms;
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for SettingWindow.xaml
+    /// </summary>
+    public partial class SettingWindow : Window
+    {
+        public SettingWindow()
+        {
+            InitializeComponent();
+        }
+        void unMute(object sender, RoutedEventArgs e)
+        {
+            string _path = Directory.GetCurrentDirectory() + "\\usersettings.json";
+            string jsonFromFile = File.ReadAllText(_path);
+            dynamic jsonObj = JsonConvert.DeserializeObject(jsonFromFile);
+            jsonObj["mute"] = null;
+            string output = JsonConvert.SerializeObject(jsonObj, Formatting.Indented);
+            File.WriteAllText(_path, output);
+            this.Close();
+        }
+        private void SettingWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            string _path = Directory.GetCurrentDirectory() + "\\usersettings.json";
+            string jsonFromFile = File.ReadAllText(_path);
+            dynamic jsonObj = JsonConvert.DeserializeObject(jsonFromFile);
+            if (jsonObj["mute"] == null)
+            {
+
+                MuteToggle.Text = "Mute: Off";
+                MuteButton.Click += new RoutedEventHandler(MuteButtonClick);
+
+            }
+            else
+            {
+                MuteToggle.Text = "Unmute Notifications";
+                MuteButton.Click += new RoutedEventHandler(unMute);
+
+            }
+        }
+        private void OnPreferenceClick(object sender, RoutedEventArgs e)
+        {
+            PreferenceWindow pw = new PreferenceWindow();
+            this.Hide();
+            pw.ShowDialog();
+            this.Show();
+        }
+        private void MuteButtonClick(object sender, RoutedEventArgs e)
+        {
+            MuteWindow mw = new MuteWindow();
+            this.Hide();
+            mw.ShowDialog();
+            this.Close();
+        }
+
+        private void uninstall_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            About abt = new About();
+            this.Hide();
+            abt.ShowDialog();
+            this.Show();
+        }
+    }
+}
